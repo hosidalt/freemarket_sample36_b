8a70d887716c28f7c512e5572fc971eab05a7212
RyujiTakami <r.takami@and1acct.jp>
2019-03-24 21:21:57 +0900

    New schema.rb
    Migration fileをrake db:migrateした結果です。

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190307124713) do
+ActiveRecord::Schema.define(version: 20190324112348) do
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "category",   null: false
+    t.integer  "parent_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "item_categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "item_id",     null: false
+    t.integer  "categoty_id", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["categoty_id"], name: "index_item_categories_on_categoty_id", using: :btree
+    t.index ["item_id"], name: "index_item_categories_on_item_id", using: :btree
+  end
+
+  create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",                     null: false
+    t.text     "introduce",  limit: 65535, null: false
+    t.integer  "price",                    null: false
+    t.integer  "seller_id",                null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["seller_id"], name: "index_items_on_seller_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
     t.string   "nickname"
+    t.string   "family_name",                                       null: false
+    t.string   "first_name",                                        null: false
+    t.string   "kana_family_name",                                  null: false
+    t.string   "kana_first_name",                                   null: false
+    t.integer  "birth_year",                                        null: false
+    t.integer  "birth_month",                                       null: false
+    t.integer  "birth_day",                                         null: false
+    t.integer  "credit",                                            null: false
+    t.string   "tel"
+    t.string   "adress"
+    t.string   "profil_image"
+    t.text     "profil_comment",         limit: 65535
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
