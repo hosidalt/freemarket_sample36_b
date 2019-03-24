f2e0f78a146bf8e0eb4fc7066b190ac22b0f8ec5
RyujiTakami <r.takami@and1acct.jp>
2019-03-24 21:21:08 +0900

    Edit User Model and add column
    User Modelを編集（association）し、必要なカラムを追加しました。

--- db/migrate/20190307124713_add_nickname_to_users.rb
+++ db/migrate/20190307124713_add_nickname_to_users.rb
@@ -1,5 +1,16 @@
 class AddNicknameToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :nickname, :string
+    add_column :users, :family_name,        :string,    null: false
+    add_column :users, :first_name,         :string,    null: false
+    add_column :users, :kana_family_name,   :string,    null: false
+    add_column :users, :kana_first_name,    :string,    null: false
+    add_column :users, :birth_year,         :integer,   null: false
+    add_column :users, :birth_month,        :integer,   null: false
+    add_column :users, :birth_day,          :integer,   null: false
+    add_column :users, :credit,             :integer,   null: false,  unique: true
+    add_column :users, :tel,                :string
+    add_column :users, :adress,             :string
+    add_column :users, :profil_image,       :string
+    add_column :users, :profil_comment,     :text
   end
 end
