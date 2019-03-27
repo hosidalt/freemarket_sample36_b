28efcd32d20a8a01b9430f30889687bdd57f68d0
RyujiTakami <r.takami@and1acct.jp>
2019-03-27 13:13:38 +0900

    Edit Item　Model association has_many :categories
    Item Modelのアソシエーション　has_many :category を　categoriesに修正しました

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  has_many    :category, through: :item_categories
+  has_many    :categories, through: :item_categories
   has_many    :item_categories
   belongs_to  :user
 end
