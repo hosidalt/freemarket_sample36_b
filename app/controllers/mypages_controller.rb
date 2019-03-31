0fc3b88c5156785321a91b2c901eecb8f329106e
RyujiTakami <r.takami@and1acct.jp>
2019-03-31 14:05:59 +0900

    Create controller Mypages
     Mypages controller を作成しました

---
+++ app/controllers/mypages_controller.rb
@@ -0,0 +1,24 @@
+class MypagesController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
