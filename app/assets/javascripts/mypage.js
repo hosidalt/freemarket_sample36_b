18fefd7bf4325ddfb9b01b8093867bf0d5d6eb62
hosidalt <itie0112@yahoo.co.jp>
2019-04-07 13:17:39 +0900

    mypage2
    re:mypage

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -1,16 +1,3 @@
-// $(function(){
-//   $(".mypage_tab").on("click", function(){
-//     var tab = $(this).data("tab");
-//     var $th = $(this).index(tab);
-//     console.log(tab);
-//     $(".mypage_tab").removeClass("active");
-//     $(".tab_panel").removeClass("active");
-//     $(this).addClass("active");
-//     $(".tab_panel").eq($th).addClass("active")
-//   });
-// });
-
-
 $(function(){
   $(".notification-todo").on("click", function(){
     var $th = $(this).index();
@@ -33,7 +20,6 @@ $(function(){
 
 $(function(){
   $(".mypage-nav__list-item").on("click", function(){
-    console.log('text')
     var index = $(".mypage-nav__list-item").index(this);
     $(".mypage-nav__list-item").removeClass("active");
     $(this).addClass("active");
