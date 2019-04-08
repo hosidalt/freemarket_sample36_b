94ce9fa1a5fc343f560446ec7b563542e8fdc519
hosidalt <itie0112@yahoo.co.jp>
2019-04-08 18:14:36 +0900

    My page5
    mypageのviewの再修正

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -6,9 +6,7 @@ $(function(){
     $(this).addClass("active");
     $(".notifi-todo_panel").eq($th).addClass("active")
   });
-});
 
-$(function(){
   $(".purchase").on("click", function(){
     var $th = $(this).index();
     $(".purchase").removeClass("active");
@@ -16,9 +14,7 @@ $(function(){
     $(this).addClass("active");
     $(".purchase_panel").eq($th).addClass("active")
   });
-});
 
-$(function(){
   $(".mypage-nav__list-item").on("click", function(){
     var index = $(".mypage-nav__list-item").index(this);
     $(".mypage-nav__list-item").removeClass("active");
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'items#index'
-  resources :mypages, only: [:index]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :mypages, only: [:index,]
+
 end
