0a364292efe484a55b7025307d421b889560772b
hosidalt <itie0112@yahoo.co.jp>
2019-04-09 19:41:12 +0900

    mypage6
    mypageの再々修正
    Side-berを部分テンプレートに

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -6,7 +6,6 @@ $(function(){
     $(this).addClass("active");
     $(".notifi-todo_panel").eq($th).addClass("active")
   });
-
   $(".purchase").on("click", function(){
     var $th = $(this).index();
     $(".purchase").removeClass("active");
@@ -14,7 +13,9 @@ $(function(){
     $(this).addClass("active");
     $(".purchase_panel").eq($th).addClass("active")
   });
+});
 
+$(function(){
   $(".mypage-nav__list-item").on("click", function(){
     var index = $(".mypage-nav__list-item").index(this);
     $(".mypage-nav__list-item").removeClass("active");
