3a350bc59dbec247e357c07a15d24b0bb4dab481
hosidalt <itie0112@yahoo.co.jp>
2019-04-11 00:04:22 +0900

    Merge remote-tracking branch 'ayukua/master'
    # Conflicts:
    #	app/assets/stylesheets/application.scss
    #	app/views/mypages/profile.html.haml
    #	config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'items#index'
+<<<<<<< HEAD
   resources :mypages, only: [:index]
+=======
+  resources :mypages, only: [:index, :edit, :update]
+>>>>>>> ayukua/master
 
 end
