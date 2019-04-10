f51daa70e6ab6252139b7bf9b7fc271cd7967d94
i__kobe <issei.o_o.iverson@icloud.com>
2019-04-10 20:22:32 +0900

    Edit unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,3 @@
-before_exec do |server|
-  ENV["BUNDLE_GEMFILE"] = File.join(File.expand_path("../../../", __FILE__), "current", "Gemfile")
-end
-
 app_path = File.expand_path('../../../', __FILE__)
 
 worker_processes 1
@@ -39,6 +35,10 @@ before_fork do |server, worker|
       logger.error e
     end
   end
+end
+
+before_exec do |server|
+  ENV["BUNDLE_GEMFILE"] = File.join(File.expand_path("../../../", __FILE__), "current", "Gemfile")
 end
 
 after_fork do |_server, _worker|
