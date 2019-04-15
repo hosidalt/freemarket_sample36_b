530eabfac5b9a23494a7471d6a80b6162d825384
hosidalt <itie0112@yahoo.co.jp>
2019-04-15 18:36:23 +0900

    Merge remote-tracking branch 'ayukua/master' into idetifications
    # Conflicts:
    #	app/assets/stylesheets/application.scss
    #	app/assets/stylesheets/mypages.scss
    #	config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'items#index'
-  resources :mypages, only: [:index]
+  resources :items
+  resources :mypages, only: [:index] do
+    resources :cards
+  end
   resources :profiles, only: [:index]
+<<<<<<< HEAD
   resources :identifications, only: [:index]
 
+=======
+  resources :users
+  resources :statuses
+>>>>>>> ayukua/master
 end
+
