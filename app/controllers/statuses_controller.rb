66df47693e576f2713cfd5d0c6ea2fcacd40c6ff
RyujiTakami <r.takami@and1acct.jp>
2019-04-19 22:33:25 +0900

    カードでの支払機能
    カードでの支払機能を実装しました

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,5 +1,7 @@
 class StatusesController < ApplicationController
 
+  require "payjp"
+
   def index
   end
 
@@ -7,6 +9,17 @@ class StatusesController < ApplicationController
   end
 
   def new
+  end
+
+  def pay
+    card = Card.where(user_id: 1).first
+    Payjp.api_key = PAYJP_SECRET_KEY
+    Payjp::Charge.create(
+      amount: params[:statuses][:amount],
+      customer: card.customer_id,
+      currency: "jpy"
+    )
+    redirect_to root_path
   end
 
   def create
