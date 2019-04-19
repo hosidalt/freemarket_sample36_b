3a150b7506f2daeffeff39a4d431a24703da6993
RyujiTakami <r.takami@and1acct.jp>
2019-04-19 22:33:06 +0900

    カードの登録機能
    カードの登録機能を実装しました

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,12 +1,46 @@
 class CardsController < ApplicationController
 
+  require "payjp"
+
   def index
   end
 
   def show
+    card = Card.where(user_id: 1).first
+    if card.blank?
+      redirect_to action: "new"
+    else
+      Payjp.api_key = PAYJP_SECRET_KEY
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @card = card
+      @default_card_information = customer.cards.retrieve(card.card_id)
+      @exp_month = @default_card_information.exp_month.to_s
+      @exp_year = @default_card_information.exp_year.to_s.slice(2,3)
+    end
   end
 
   def new
+    card = Card.where(user_id: 1)
+    redirect_to mypage_card_path(1, 1) if card.exists?
+  end
+
+  def create_card
+    Payjp.api_key = PAYJP_SECRET_KEY
+    if params[:payjp_token].blank?
+      redirect_to new_mypage_card_path(:mypage_id)
+    else
+      customer = Payjp::Customer.create(
+        description: "登録テスト",
+        card: params[:payjp_token],
+        metadata: {user_id: 1}
+      )
+      @card = Card.new(user_id: 1, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+        redirect_to mypage_card_path(1, 1)
+      else
+        redirect_to new_mypage_card_path(:mypage_id)
+      end
+    end
   end
 
   def create
@@ -18,7 +52,15 @@ class CardsController < ApplicationController
   def update
   end
 
-  def destroy
+  def delete
+    card = Card.where(user_id: 1).first
+    if card.blank?
+    else
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer.delete
+      card.delete
+    end
+    redirect_to new_mypage_card_path(1, 1)
   end
 
 end
