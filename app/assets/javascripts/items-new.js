5eed72f888c28b10f4817606504ceec4ed11f0d5
i__kobe <issei.o_o.iverson@icloud.com>
2019-04-23 15:33:32 +0900

    Edit items-new.js

--- app/assets/javascripts/items-new.js
+++ app/assets/javascripts/items-new.js
@@ -6,10 +6,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
   var imagesContainer = document.getElementById('item-images-container');
   var imagesLists2 = document.getElementById('images-lists2');
   var imagesContainer2 = document.getElementById('item-images-container2');
-  var num = 0;
-  var priceInput = document.getElementById('price-input');
-  var commissionResult = document.getElementById('commission-result');
-  var benefitResult = document.getElementById('benefit-result');
+  var num = 0; //input file内の画像の枚数を把握
 
   function handleFileSelect(evt) {
     var files = evt.target.files;
@@ -50,8 +47,13 @@ document.addEventListener('DOMContentLoaded', function(e) {
               this.parentNode.parentNode.remove();
               this.parentNode.remove();
               this.remove();
+              //thisを最後に削除しないと、parentNodeなどが認識されなくなる
               num--;
-              postArea.classList.add(`have-${num}-item`)
+              postArea.classList.add(`have-${num}-item`);
+              if ( num < 6 ) {
+                imagesLists2.classList.add('hidden');
+                imagesContainer2.classList.add('hidden');
+              }
             }, false);
             return li;
           };
@@ -104,6 +106,106 @@ document.addEventListener('DOMContentLoaded', function(e) {
   function separate(num){
     return String(num).replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
   }
+
+  var firstCategory = document.getElementById('first-category-select');
+  var categoryForm = document.getElementById('category-form');
+
+  firstCategory.addEventListener('change', function() {
+    if (this.value !== "" && document.getElementById('second-category') === null) {
+      var selectWrap = document.createElement('div');
+      selectWrap.classList.add('select-wrap');
+      selectWrap.innerHTML = `<select id="second-category">
+      <option value="">---</value>
+      <option value="レディース">レディース</value>
+      <option value="メンズ">メンズ</value>
+      <option value="ベビー・キッズ">ベビー・キッズ</value>
+      <option value="インテリア・住まい・小物">インテリア・住まい・小物</value>
+      <option value="本・音楽・ゲーム">本・音楽・ゲーム</value>
+      </select>`;
+      categoryForm.appendChild(selectWrap);
+      var secondCategory = document.getElementById('second-category');
+      console.log(secondCategory);
+      secondCategory.addEventListener('change', function() {
+        if (this.value !== "" && document.getElementById('third-category') === null) {
+          var selectWrap = document.createElement('div');
+          selectWrap.classList.add('select-wrap');
+          selectWrap.innerHTML = `<select id="third-category">
+          <option value="">---</value>
+          <option value="">レディース</value>
+          <option value="">メンズ</value>
+          <option value="">ベビー・キッズ</value>
+          <option value="">インテリア・住まい・小物</value>
+          <option value="">本・音楽・ゲーム</value>
+          </select>`;
+          var thirdCategory = document.getElementById('third-category');
+          categoryForm.appendChild(selectWrap);
+        };
+      });
+      secondCategory.addEventListener('change', function() {
+        var thirdCategory = document.getElementById('third-category');
+        if (this.value === "" && thirdCategory != null) {
+          thirdCategory.parentNode.remove();
+          thirdCategory.remove();
+        };
+      });
+    }else if (this.value === "") {
+      var secondCategory = document.getElementById('second-category');
+      secondCategory.parentNode.remove();
+      secondCategory.remove();
+      if (document.getElementById('third-category') !== null) {
+        var thirdCategory = document.getElementById('third-category');
+        thirdCategory.parentNode.remove();
+        thirdCategory.remove();
+      };
+    };
+  });
+  //データを引っ張った時に必要になる関数。
+  // function getSelectLabel(idname){
+  //   console.log(idname);
+  //   var idx = idname.selectedIndex;       //インデックス番号を取得
+  //   var val = idname.options[idx].value;  //value値を取得
+  //   var txt  = idname.options[idx].text;  //ラベルを取得
+  //   console.log(idx);
+  //   console.log(val);
+  //   console.log(txt);
+  // };
+
+  var deliveryCost = document.getElementById('delivery-cost');
+  var deliveryBox = document.getElementById('delivery-box');
+  deliveryCost.addEventListener('change', function() {
+    if (this.value !== "" && document.getElementById('how-to-delivery') === null) {
+      var formHowDelivery = document.createElement('div');
+      formHowDelivery.classList.add('form-group');
+      var h3 = document.createElement('h3');
+      h3.textContent = "配送の方法";
+      var span = document.createElement('span');
+      span.textContent = "必須";
+      h3.appendChild(span);
+      formHowDelivery.appendChild(h3);
+      var selectWrap = document.createElement('div');
+      selectWrap.classList.add('select-wrap');
+      selectWrap.innerHTML = `<select id="how-to-delivery">
+      <option value="">---</value>
+      <option value="0">未定</value>
+      <option value="1">らくらくメルカリ便</value>
+      <option value="2">ゆうメール</value>
+      <option value="3">レターパック</value>
+      <option value="4">普通郵便（定型・定型外）</value>
+      <option value="5">クロネコヤマト</value>
+      <option value="6">ゆうパック</value>
+      <option value="7">クリックポスト</value>
+      <option value="8">ゆうパケット</value>
+      </select>`;
+      deliveryBox.appendChild(formHowDelivery);
+      deliveryBox.appendChild(selectWrap);
+    }else if (this.value === "" && document.getElementById('how-to-delivery') !== null) {
+      document.getElementById('how-to-delivery').parentNode.previousElementSibling.remove();
+      document.getElementById('how-to-delivery').parentNode.remove();
+      document.getElementById('how-to-delivery').remove();
+    };
+  });
+
+
 
 });
 
