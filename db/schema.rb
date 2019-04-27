7d77487c2ce41cfbb467d315963b10d98ae64b03
i__kobe <issei.o_o.iverson@icloud.com>
2019-04-27 19:26:48 +0900

    Edit migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190426102021) do
+ActiveRecord::Schema.define(version: 20190427095904) do
 
   create_table "cards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id",     null: false
@@ -79,7 +79,7 @@ ActiveRecord::Schema.define(version: 20190426102021) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
-    t.string   "nickname",                                          null: false
+    t.string   "nickname"
     t.string   "family_name",                                       null: false
     t.string   "first_name",                                        null: false
     t.string   "kana_family_name",                                  null: false
