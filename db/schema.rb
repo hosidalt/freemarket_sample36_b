e0d5d35cad9722ccc2ed1109bfdebf3b4e2ec125
RyujiTakami <r.takami@and1acct.jp>
2019-04-27 19:03:25 +0900

    Create Users tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190420133253) do
+ActiveRecord::Schema.define(version: 20190427095904) do
 
   create_table "cards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id",     null: false
@@ -63,13 +63,29 @@ ActiveRecord::Schema.define(version: 20190420133253) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.string   "nickname",                                          null: false
+    t.string   "family_name",                                       null: false
+    t.string   "first_name",                                        null: false
+    t.string   "kana_family_name",                                  null: false
+    t.string   "kana_first_name",                                   null: false
+    t.integer  "birth_year",                                        null: false
+    t.integer  "birth_month",                                       null: false
+    t.integer  "birth_day",                                         null: false
+    t.string   "postal_code",                                       null: false
+    t.string   "prefecture",                                        null: false
+    t.string   "city",                                              null: false
+    t.integer  "block_number",                                      null: false
+    t.string   "building_name"
+    t.string   "tel"
+    t.string   "profil_image"
+    t.text     "profil_comment",         limit: 65535
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
