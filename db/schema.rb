ba1d651dc1bd390ee41c3136cc34c0c18084bb0e
RyujiTakami <r.takami@and1acct.jp>
2019-04-27 18:56:40 +0900

    Delete database

--- db/schema.rb
+++ db/schema.rb
@@ -70,7 +70,6 @@ ActiveRecord::Schema.define(version: 20190420133253) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
