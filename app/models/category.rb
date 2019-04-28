9a0b0c18f96e8e3519eaf37748c53be469b7271a
RyujiTakami <r.takami@and1acct.jp>
2019-04-28 17:04:38 +0900

    CSV一括インポート
    レビュー不要です

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,19 @@
 class Category < ApplicationRecord
   has_many :items, through: :item_categories
   has_many :item_categories
+
+  require "csv"
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      obj = new
+      obj.attributes = row.to_hash.slice(*updatable_attributes)
+      obj.id = row.to_hash.values[0]
+      obj.save!
+    end
+  end
+
+  def self.updatable_attributes
+    ["id","category","ancestry"]
+  end
+  
 end
