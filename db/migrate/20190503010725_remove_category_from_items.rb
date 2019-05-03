1176f3571a715a35538da10c7327b019c04431c4
i__kobe <issei.o_o.iverson@icloud.com>
2019-05-03 11:24:51 +0900

    Delete migrationfile

--- db/migrate/20190503010725_remove_category_from_items.rb
+++ db/migrate/20190503010725_remove_category_from_items.rb
@@ -2,6 +2,5 @@ class RemoveCategoryFromItems < ActiveRecord::Migration[5.0]
   def change
     remove_foreign_key :items, :categories
     remove_foreign_key :items, :categories
-    remove_reference :items, :category
   end
 end
