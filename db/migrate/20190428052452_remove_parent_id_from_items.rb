e465f2e945c9c2cabe57bb381064a0a713338530
i__kobe <issei.o_o.iverson@icloud.com>
2019-05-03 11:01:44 +0900

    Delete migrationFiles

--- db/migrate/20190428052452_remove_parent_id_from_items.rb
+++
@@ -1,5 +0,0 @@
-class RemoveParentIdFromItems < ActiveRecord::Migration[5.0]
-  def change
-    remove_reference :items, :parent_category
-  end
-end
--- db/migrate/20190503010510_remove_foreign_from_items.rb
+++
@@ -1,5 +0,0 @@
-class RemoveForeignFromItems < ActiveRecord::Migration[5.0]
-  def change
-    remove_foreign_key :items, :categories
-  end
-end
