99db45690a5b4a535fdf013ad4e1aab52b0d968a
i__kobe <issei.o_o.iverson@icloud.com>
2019-05-03 11:23:48 +0900

    Delete index

--- db/migrate/20190503010725_remove_category_from_items.rb
+++ db/migrate/20190503010725_remove_category_from_items.rb
@@ -2,9 +2,6 @@ class RemoveCategoryFromItems < ActiveRecord::Migration[5.0]
   def change
     remove_foreign_key :items, :categories
     remove_foreign_key :items, :categories
-    remove_index :items, :parent_category_id
-    remove_index :items, :child_category_id
-    remove_index :items, :grandchild_category_id
     remove_reference :items, :category
   end
 end
