e3273d09896a44e014f2a049d80f546f49a425af
RyujiTakami <r.takami@and1acct.jp>
2019-05-03 17:45:21 +0900

    delete db

--- db/migrate/20190503082613_remove_name_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveNameFromUsers < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :users, :name, :string
-    remove_column :users, :credit, :integer
-    remove_column :users, :token, :string
-    remove_column :users, :provider, :string
-    remove_column :users, :uid, :string
-  end
-end
