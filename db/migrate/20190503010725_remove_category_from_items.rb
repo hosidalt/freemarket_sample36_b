9d382db557ba062b98a732c7dfd204840d5de3ca
ayukua <hamada@hamadanoMacBook-puro.local>
2019-05-03 12:54:09 +0900

    Remove category

--- db/migrate/20190503010725_remove_category_from_items.rb
+++ db/migrate/20190503010725_remove_category_from_items.rb
@@ -1,5 +1,10 @@
 class RemoveCategoryFromItems < ActiveRecord::Migration[5.0]
   def change
+    remove_foreign_key :items, :categories
+    remove_foreign_key :items, :categories
+    remove_foreign_key :items, :categories
+    remove_foreign_key :items, :users
+    remove_index :items, :parent_category_id
     remove_index :items, :child_category_id
     remove_index :items, :grandchild_category_id
     remove_index :items, :seller_id
