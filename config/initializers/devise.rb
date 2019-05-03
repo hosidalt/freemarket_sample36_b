ed0ffe0e0c86f5452345f63adb669aeb25162713
hosidalt <itie0112@yahoo.co.jp>
2019-05-03 15:09:30 +0900

    Merge remote-tracking branch 'ayukua/master'

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -257,6 +257,15 @@ Devise.setup do |config|
   config.sign_out_via = :delete
 
   # ==> OmniAuth
+
+  Devise.setup do |config|
+
+    config.omniauth( :google_oauth2,
+                     ENV['GOOGLE_CLIENT_ID'],
+                     ENV['GOOGLE_CLIENT_SECRET'],
+                     {:scope => 'email', :redirect_uri => "http://localhost:3000/users/auth/google_oauth2/callback"})
+  end
+
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
