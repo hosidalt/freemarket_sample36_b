55c76d72cbe945e46e310513fa03ffc988817702
RyujiTakami <r.takami@and1acct.jp>
2019-05-07 19:52:56 +0900

    Edit product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
-  has_many :images, foreign_key: "item_id"
-  has_many :statuses, foreign_key: "item_id"
-  has_many :item_categories, foreign_key: "item_id"
+  has_many :images, foreign_key: "item_id", dependent: :delete_all
+  has_many :statuses, foreign_key: "item_id", dependent: :delete_all
+  has_many :item_categories, foreign_key: "item_id", dependent: :delete_all
   belongs_to :user
 end
