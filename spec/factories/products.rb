d1e4575da4a0f0b94064cfa5ba1be0e9d3fe1b70
RyujiTakami <r.takami@and1acct.jp>
2019-05-08 21:36:04 +0900

    User product factory bot create

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :product do
-    
+    name                    {"カバン"}
+    introduce               {"商品紹介"}
+    condition               {"1"}
+    delivery_fee_payer      {"1"}
+    shipping_method         {"1"}
+    area                    {"1"}
+    days_to_delivery        {"1"}
+    price                   {"1000"}
+    parent_category_id      {"1"}
+    child_category_id       {"11"}
+    grandchild_category_id  {"301"}
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,21 @@
 FactoryBot.define do
-
   factory :user do
+    id                    {"1"}
     nickname              {"abe"}
     email                 {"kkk@gmail.com"}
     password              {"00000000"}
     password_confirmation {"00000000"}
+    family_name           {"山田"}
+    first_name            {"太郎"}
+    kana_family_name      {"ヤマダ"}
+    kana_first_name       {"タロウ"}
+    birth_year            {"2000"}
+    birth_month           {"1"}
+    birth_day             {"20"}
+    postal_code           {"111-1111"}
+    prefecture            {"兵庫県"}
+    city                  {"神戸市"}
+    block_number          {"1"}
   end
 
 end
