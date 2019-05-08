3ead70ba3135265fe7477b42cf43b845cf665b6e
hosidalt <itie0112@yahoo.co.jp>
2019-05-08 23:16:42 +0900

    テストコード

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe "GET #show" do
+    # userをFactoryBotで作る
+    let(:user) { FactoryBot.build(:user) }
+    # 作ったユーザでログインする
+    before do
+      login_user user
+    end
 
+    it "can get it" do
+      get :show, id: 1
+      # 出力結果
+      p response
+    end
+  end
 end
--- spec/models/sns_credential_spec.rb
+++ spec/models/sns_credential_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
+describe SnsCredential do
 
-RSpec.describe SnsCredential, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '登録できる場合' do
+    it "name, email, provider, uid が全てnilでない" do
+      sns_credential = build(:sns_credential)
+      expect(sns_credential).to be_valid
+    end
+  end
+
+  describe "登録できない場合" do
+    it "nameがnilの場合は登録できない" do
+      sns_credential = build(:sns_credential, name: nil)
+      sns_credential.valid?
+      expect(sns_credential.errors[:name]).to include("can't be blank")
+    end
+
+    it "eamilがnilの場合は登録できない" do
+      sns_credential = build(:sns_credential, email: nil)
+      sns_credential.valid?
+      expect(sns_credential.errors[:email]).to include("can't be blank")
+    end
+
+    it "providerがnilの場合は登録できない" do
+      sns_credential = build(:sns_credential, provider: nil)
+      sns_credential.valid?
+      expect(sns_credential.errors[:provider]).to include("can't be blank")
+    end
+
+    it "uidがnilの場合は登録できない" do
+      sns_credential = build(:sns_credential, uid: nil)
+      sns_credential.valid?
+      expect(sns_credential.errors[:uid]).to include("can't be blank")
+      end
+
+    it "emailが重複する場合は登録できない" do
+      sns_credential = create(:sns_credential)
+      another_user = build(:sns_credential, email: sns_credential.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end
+  end
 end
+
