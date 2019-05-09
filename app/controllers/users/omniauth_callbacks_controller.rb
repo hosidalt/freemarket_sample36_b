de8f9a86447ba42a7476c105d658bd6df052337d
hosidalt <itie0112@yahoo.co.jp>
2019-05-09 20:13:26 +0900

    sns認証
    Sns認証関係のディレクトリ

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,31 +1,38 @@
-class Users::OmniauthCallbacksController < ApplicationController
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  # def facebook
-  #   @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
 
-  #   if @user.persisted?
-  #     set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-  #     sign_in_and_redirect @user, :event => :authentication
-  #   else
-  #     session["devise.facebook_data"] = request.env["omniauth.auth"]
-  #     redirect_to new_user_registration_url
-  #   end
-  # end
+  def facebook
+    @user = SnsCredential.find_for_facebook(request.env["omniauth.auth"])
 
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication
+      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      render template: 'devise/registrations/new_sns'
+    end
+  end
+  def failure
+    redirect_to root_path and return
+  end
 
 
 
   def google_oauth2
-    @user = SnsCredential.find_for_google_oauth2(request.env["omniauth.auth"])
-
-
+    @user = SnsCredential.find_for_google_oauth2(request.env["omniauth.auth"].except(:extra))
 
     if @user.persisted?
-      set_flash_message(:notice, :success, :kind => "google_oauth2") if is_navigational_format?
       sign_in_and_redirect @user, :event => :authentication
+      set_flash_message(:notice, :success, :kind => "google_oauth2") if is_navigational_format?
+
     else
       session["devise.google_oauth2_data"] = request.env["omniauth.auth"].except("extra")
-      redirect_to new_user_registration_url
+      render template: 'devise/registrations/new_sns'
+
     end
+  end
+  def failure
+    redirect_to root_path and return
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2, :facebook]
   validates :nickname, presence: true, length: { maximum: 20 }
 
   has_many :cards
+  has_many :sns_credentials, dependent: :destroy
 end
 
