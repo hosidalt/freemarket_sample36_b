287b68f0545f8b06afd120b2028562b1163ed468
ayukua <hamada@hamadanoMacBook-puro.local>
2019-05-09 21:05:22 +0900

    アソシエーション変更

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,6 @@ class Product < ApplicationRecord
   has_many :images, foreign_key: "item_id", dependent: :delete_all
   has_many :statuses, foreign_key: "item_id", dependent: :delete_all
   has_many :item_categories, foreign_key: "item_id", dependent: :delete_all
-  belongs_to :user
   scope :get_random_product, -> (id){ where("parent_category_id = ?", id).order("RAND()").limit(4) }
   belongs_to :user, foreign_key: "seller_id"
   validates :name, :introduce, :condition, :delivery_fee_payer, :shipping_method, :area, :days_to_delivery, :price, :seller_id, :parent_category_id, :child_category_id, :grandchild_category_id, presence: true
